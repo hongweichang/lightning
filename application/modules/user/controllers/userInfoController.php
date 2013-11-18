200c11dbbb3198f88d797b13b46d28e5945e2084
tianling <2507073658@qq.com>
2013-11-19 01:56:27 +0800

    upload and filetype get

--- application/modules/user/controllers/userInfoController.php
+++ application/modules/user/controllers/userInfoController.php
@@ -40,56 +40,67 @@ class userInfoController extends CmsController{
 
 	public function actionUpload(){
 		$typeArray = array('jpg','png','gif','jpeg','pdf','zip','rar');
-			$maxSize = 1024*1024*30; //最大文件大小约为30MB
+		$maxSize = 1024*1024*30; //最大文件大小约为30MB
+
 			if(isset($_FILES['Filedata'])){
 				$fileInfo = CUploadedFile::getInstanceByName('Filedata');
-				//var_dump($fileInfo);
-				//die($fileInfo->type);
 				$fileName = $fileInfo->name;
 				$fileType =  pathinfo($fileName, PATHINFO_EXTENSION); 
-				//die($fileType);
-				//$thumbDir = dirname(Yii::app()->basePath)."/upload/ad_pic/thumbs/";
-				$uploadDir = dirname(Yii::app()->basePath)."/upload/";
+
+				$uploadDir = dirname(Yii::app()->basePath)."/upload/".$fileType.'/';
 				$dateDir = date('Ym')."/";
 				$uploadDir = $uploadDir.$dateDir;
-				//$thumbDir = $thumbDir.$dateDir;
-				if(!is_dir($uploadDir)){
+
+				if(!is_dir($uploadDir)){ //若目标目录不存在，则生成该目录
 						mkdir($uploadDir,0077,true);
 				}
-				/*if(!is_dir($thumbDir)){
-					mkdir($thumbDir,0077,true);
-				}*/
 				
 				$randName = Tool::getRandName();//获取一个随机名
 				$newName = "userInfo".$randName.".".$fileName;//对文件进行重命名
 				$saveUrl = $uploadDir.$newName;
-				$picUrl = "/upload/ad_pic/pics/".$dateDir.$newName;
 				$isUp = $fileInfo->saveAs($saveUrl);//保存上传文件
 				if($isUp){
-					echo "ok";
-					die();
-					/*$thumbName = "thumbs".$randName.".".$picType;
-					$saveThumb = $thumbDir.$thumbName;
-					$thumbUrl = Tool::getThumb($saveUrl,300,300,$saveThumb);//制作缩略图并放回缩略图存储路径
-					$thumbUrl = str_replace(dirname(Yii::app()->basePath),"",$thumbUrl);*/
-					
-					//保存信息到数据库
-					$model = new AdvertisePic;
-					$model->url = $picUrl;
-					$model->description = $picName;
-					$model->thumb_url = $thumbUrl;
-					$model->save();
-					$id = $model->attributes['id'];
-					$_SESSION['pid'] = $id;
+					//获取对应类型
+					$typeName = $this->getFileType($fileType);	
+					$url = $saveUrl;
+
+					//保存信息到SESSION
+					$_SESSION['typeName'] = $typeName;
+					$_SESSION['url'] = $saveUrl;
+
 					$backData = array(
-						'pid'=>$id,
-						'thumb'=>Yii::app()->baseUrl.$thumbUrl,
+						'name'=>$fileName,
+						//'thumb'=>Yii::app()->baseUrl.$thumbUrl,
 						);
 					// 返回json数据给swfupload上传插件
 					echo  json_encode($backData);
 				}
 			}
 
+	}
+
+
+	/*
+	**获取上传文件在数据库中对应的type
+	*/
+	public function getFileType($fileType){
+
+		if(isset($fileType) && !empty($fileType)){
+			$map = array(
+					'file'=>array('pdf','zip','rar'),
+					'image'=>array('jpg','jpeg','png','gif')
+				);
+
+			foreach($map as $type=>$typeData){
+				foreach($typeData as $key=> $value){
+					if(preg_match('/'.$value.'/',$fileType)){
+						return $type; 
+					}
+				}
+				
+			}
+			return 400;
+		}
 	}
 }
 ?>
