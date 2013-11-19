a3f990ff2e8c96678e3088c692bad83d52b2193b
toruneko <toruneko@outlook.com>
2013-11-19 20:42:41 +0800

    x

--- application/controllers/TestController.php
+++ application/controllers/TestController.php
@@ -6,17 +6,9 @@
  * Encoding UTF-8
  */
 class TestController extends CmsController{
-<<<<<<< HEAD
 	public function filters(){
 		return array();
 	}
-=======
-
-	public function filters(){
-		return array();
-	}
-	
->>>>>>> tmp
 	public function actionIndex(){
 		echo 'succed';
 	}
