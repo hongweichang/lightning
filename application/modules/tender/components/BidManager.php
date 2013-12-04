4962780ca29708fa31f7e22d0104b644d976d7ed
toruneko <toruneko@outlook.com>
2013-12-04 22:48:12 +0800

    xx

--- application/modules/tender/components/BidManager.php
+++ application/modules/tender/components/BidManager.php
@@ -17,7 +17,7 @@ class BidManager extends CApplicationComponent{
 	}
 	
 	public function getBidMetaInfo($metaId,$condition='',$params=array()){
-		return BidMeta::model()->with('user','bid')->findByPk($metaId,$condition,$params);
+		return BidMeta::model()->with(array('user','bid'))->findByPk($metaId,$condition,$params);
 	}
 	
 	/**
@@ -35,7 +35,7 @@ class BidManager extends CApplicationComponent{
 	 * @param array $params
 	 */
 	public function getBidMetaList($condition, $params = array()){
-		return BidMeta::model()->with('user','bid')->findAll($condition,$params);
+		return BidMeta::model()->with(array('user','bid'))->findAll($condition,$params);
 	}
 	
 	/**
@@ -107,7 +107,7 @@ class BidManager extends CApplicationComponent{
 	 * @return boolean
 	 */
 	public function payPurchaseBid($meta_no){
-		$meta = BidMeta::model()->with('user','bid')->findByPk($meta_no);
+		$meta = BidMeta::model()->with(array('user','bid'))->findByPk($meta_no);
 		$user = $meta->getRelated('user');
 		
 		$transaction = Yii::app()->db->beginTransaction();
@@ -135,7 +135,7 @@ class BidManager extends CApplicationComponent{
 	 * @return boolean
 	 */
 	public function revokePurchaseBid($meta_no){
-		$meta = BidMeta::model()->with('user','bid')->findByPk($meta_no);
+		$meta = BidMeta::model()->with(array('user','bid'))->findByPk($meta_no);
 		$user = $meta->getRelated('user');
 		$bid = $meta->getRelated('bid');
 		
