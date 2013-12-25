0c29735bf4bb137feb69a05423044bfba02728ad
Lancelot <lancelot1215@gmail.com>
2013-12-25 21:19:59 +0800

    css

--- application/modules/content/views/article/view.php
+++ application/modules/content/views/article/view.php
@@ -9,8 +9,8 @@ $this->cs->registerCssFile($this->cssUrl.'help.css');
 ?>
 
 <div class="wd1002 hc clearfix">
-	<div id="hc-content">
-                <h2><?php echo $article->title;?></h2>
+	<div id="hc-detail-content">
+                <h2 id="title"><?php echo $article->title;?></h2>
                 <?php echo $article->content?>
 	</div>
 </div>
