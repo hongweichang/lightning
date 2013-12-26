a3c04214756467f60aaac59446cebec33e5a9ec7
ArborLin <vlinyq@gmail.com>
2013-12-26 13:30:26 +0800

    x

--- application/modules/tender/views/purchase/index.php
+++ application/modules/tender/views/purchase/index.php
@@ -98,9 +98,16 @@ $this->cs->registerScriptFile($this->scriptUrl.'lend.js',CClientScript::POS_END)
 					'cssFile' => false,
 					'baseScriptUrl' => null,
 				)); ?>
-                <div id="viewMore">
+                <!-- <div id="viewMore">
                 	<img src="<?php echo $this->imageUrl;?>more_ico.png" />
                 	<span>查看更多</span>
-                </div>
+                </div> -->
+                <ul id="page">
+                    <li><a href="#" class="page-prev">上一页</a></li>
+                    <li><a href="#" class="active">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#" class="page-next">下一页</a></li>
+                </ul>
             </div>
     	</div>
