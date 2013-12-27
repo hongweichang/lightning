10fad223ea75995397c18265270f79636a46e4a0
toruneko <toruneko@outlook.com>
2013-12-27 20:53:55 +0800

    x

--- application/commands/BidCommand.php
+++ application/commands/BidCommand.php
@@ -34,12 +34,40 @@ class BidCommand extends LightningCommandBase{
 		$bids = $db->getBidList(array(
 			'condition' => 'verify_progress=31',
 		));
+
+		$date = mktime(0,0,0);
+		$h31d = array(1,3,5,7,8,10,12);
+		$h30d = array(4,6,9,11);
 		
-		$time = time();
 		foreach($bids as $bid){
-			$month = floor(($time - $bid->getAttribute('repay_time')) / 86400 / 30); // 怎么提前？
-			if($bid->getAttribute('repay_deadline') + $month > $bid->getAttribute('deadline')){
-				//@TODO 发通知哦~
+			$repay_time = strtotime(date('Y-n-j',$bid->getAttribute('repay_time')));
+			list($year,$month,$day) = explode('-', date('Y-n-j',$date - $repay_time));
+
+			$isSend = false;
+			if($bid->getAttribute('repay_deadline') + $month - 1 > $bid->getAttribute('deadline')){
+				if($day >=3){
+					$db->finishBid($bid,true); // 逾期
+				}else{
+					$isSend = true;
+				}
+			}else{
+				if(in_array($month, $h31d) && $day >= 29){
+					$isSend = true;
+				}else if(in_array($month, $h30d) && $day >= 28){
+					$isSend = true;
+				}else if($month == 2 && $day >= 26){
+					if($year % 4 == 0 && ($year % 100 != 0 || $year % 400 == 0)){
+						if($day >= 27){
+							$isSend = true;
+						}
+					}else{
+						$isSend = true;
+					}
+				}
+			}
+			
+			if($isSend){
+				//@TODO 发送通知
 			}
 		}
 	}
