766fa2e724e9e3c0d2cbcadaf784f80f6a32319c
ArborLin <vlinyq@gmail.com>
2013-12-27 20:15:15 +0800

    modify

--- application/modules/content/views/help/index.php
+++ application/modules/content/views/help/index.php
@@ -1,39 +1,45 @@
 <?php $this->cs->registerCssFile($this->cssUrl.'help.css'); 
 $this->cs->registerScriptFile($this->scriptUrl.'help.js',CClientScript::POS_END);
 ?>
+    <div class="wd1002 outer">
          <div class="wd1002 hc clearfix">
-            <div class="hc-side about-side">
-                <ul>
-                	<?php 
-                	$cid = $this->activeCategory->id;
-                	foreach ($this->categories as $val):
-                		$name = $val->category_name;
-                		if ( mb_strlen($name,'UTF-8') > 6 ){
-                			$name = mb_substr($name,0,6,'UTF-8').'...';
-                		}
-                	?>
-                    <li class="hc-side-item" title="<?php echo $val->category_name?>">
-                    	<a href="<?php echo $this->createUrl('',array('cid'=>$val->id))?>" <?php if($val->id == $cid) echo ' class="active"';?>>
-                    	<?php echo $name?>
-                    	</a>
-                    </li>
-                    
-                    <?php endforeach;?>
-                </ul>
-            </div>
-            <div id="hc-content">
-            	<div class="about-title">
-                    <h2><?php echo $this->activeCategory->category_name;?></h2>
+            <div class="container1">
+                <div class="hc-side about-side">
+                    <ul>
+                    	<?php 
+                    	$cid = $this->activeCategory->id;
+                    	foreach ($this->categories as $val):
+                    		$name = $val->category_name;
+                    		if ( mb_strlen($name,'UTF-8') > 6 ){
+                    			$name = mb_substr($name,0,6,'UTF-8').'...';
+                    		}
+                    	?>
+                        <li class="hc-side-item" title="<?php echo $val->category_name?>">
+                        	<a href="<?php echo $this->createUrl('',array('cid'=>$val->id))?>" <?php if($val->id == $cid) echo ' class="active"';?>>
+                        	<?php echo $name?>
+                        	</a>
+                        </li>
+                        
+                        <?php endforeach;?>
+                    </ul>
                 </div>
-                <ul>
-                    <?php
-                    	foreach ($article as $i => $val):
-                    ?>
-                    <li>
-                        <div class="hc-title"><?php echo ($i+1).".".$val->title;?></div>
-                        <div class="hc-hidden"><?php echo $val->content;?></div>
-                    </li>
-                    <?php endforeach;?>
-                </ul>
+                <div id="hc-content">
+                    <div id="contentInner">
+                    	<div class="about-title">
+                            <h2><?php echo $this->activeCategory->category_name;?></h2>
+                        </div>
+                        <ul>
+                            <?php
+                            	foreach ($article as $i => $val):
+                            ?>
+                            <li>
+                                <div class="hc-title"><?php echo ($i+1).".".$val->title;?></div>
+                                <div class="hc-hidden"><?php echo $val->content;?></div>
+                            </li>
+                            <?php endforeach;?>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
--- application/views/site/index.php
+++ application/views/site/index.php
@@ -18,105 +18,104 @@ $bidProgressCssClassMap = $this->app['bidProgressCssClassMap'];
        <?php endforeach;?>
        </ul>
     </div>
-    <div id="container">
-        <div class="wd1002">
-            <div class="intro clearfix">
-                <a href="#">
-                    <span class="intro-pic intro-pic-1"></span>
-                    <span class="intro-title">安全保障</span>
-                    <span class="intro-text">成为理财人，通过主动投标或加入优选理财计划将资金进行出借投资，可获得预期1214%的稳定年化收益。我们会教会你所有网站的操作流程等一系列的措施。</span>
-                </a>
-                <a href="#">
-                    <span class="intro-pic intro-pic-2"></span>
-                    <span class="intro-title">快速成交</span>
-                    <span class="intro-text">优化系统结构，控制访问路径，将非法访问禁止在外。系统文件比对，防止伪装木马。高强度加密方式防破解，每套程序都有自已唯一的key，保障数据安全</span>
-                </a>
-                <a href="#">
-                    <span class="intro-pic intro-pic-3"></span>
-                    <span class="intro-title">诚信服务</span>
-                    <span class="intro-text">我手上有闲钱，想要拿出来理财，我要借出的具体细节。我急需要用钱，想要借款，我要借入的的具体细节。管理我的的账户，近期参与活动，账户安全管理，收益及余额查询等。</span>
-                </a>
+    <div class="wd1002">
+        <div class="intro clearfix">
+            <a href="#">
+                <span class="intro-pic intro-pic-1"></span>
+                <span class="intro-title">安全保障</span>
+                <span class="intro-text">拥有金融精英团队，对客户资格严格审核、有效监控、跟踪服务，以确保资金安全；拥有网络专业团队，对您的信息加密保护，通过第三方支付平台交易，确保您账户安全。</span>
+            </a>
+            <a href="#">
+                <span class="intro-pic intro-pic-2"></span>
+                <span class="intro-title">快速成交</span>
+                <span class="intro-text">申请、交易过程步骤少、操作简单；专业团队对信息进行及时反应、快速处理，使您享受犹如闪电般的投融资服务。</span>
+            </a>
+            <a href="#">
+                <span class="intro-pic intro-pic-3"></span>
+                <span class="intro-title">诚信服务</span>
+                <span class="intro-text">平台交易内容及过程真实可信；本着长期经营，诚信为本的宗旨，为您提供真实、透明的真诚服务。</span>
+            </a>
+        </div>
+        
+        <div class="news clearfix">
+            <div class="title">
+            <h1>最新公告</h1>
+            <a href="#">更多</a>
             </div>
-            
-            <div class="news clearfix">
-                <div class="title">
-                <h1>最新公告</h1>
-                <a href="#">更多</a>
-                </div>
-                <ul>
-                <?php 
-           	    $today = mktime(0,0,0);     
-                foreach ( $articles as $article ):
-                $addTime = $article->add_time;
-                $content = $article->content;;  
-           ?>
-                    <li>
-                        <p class="news-time"><?php echo date('Y-m-d',$addTime)?></p>
-                        <p class="news-liststyle"></p>
-                        <div class="news-list">
-                            <a href="<?php echo $this->createUrl('content/article/view',array('id'=>$article->id))?>">
-                   	    <?php echo $article->title?>        
-                            </a>
-                            <!-- <div class="subText text-overflow"><?php echo $content?></div> -->
-                            <?php if ( $addTime >= $today ):?>
-                            <div class="news-tips"></div>
-                            <?php endif;?>
-                        </div>
-                    </li>
-               <?php endforeach;?>
-                </ul>
+            <ul>
+            <?php 
+       	    $today = mktime(0,0,0);     
+            foreach ( $articles as $article ):
+            $addTime = $article->add_time;
+            $content = $article->content;;  
+       ?>
+                <li>
+                    <p class="news-time"><?php echo date('Y-m-d',$addTime)?></p>
+                    <p class="news-liststyle"></p>
+                    <div class="news-list">
+                        <a href="<?php echo $this->createUrl('content/article/view',array('id'=>$article->id))?>">
+               	    <?php echo $article->title?>        
+                        </a>
+                        <!-- <div class="subText text-overflow"><?php echo $content?></div> -->
+                        <?php if ( $addTime >= $today ):?>
+                        <div class="news-tips"></div>
+                        <?php endif;?>
+                    </div>
+                </li>
+           <?php endforeach;?>
+            </ul>
+        </div>
+        <div class="loan">
+            <div class="title">最新投资</div>
+            <div class="list-head">
+                <span>借款人</span>
+                <span>借款标题</span>
+                <span>年利率</span>
+                <span>信用等级</span>
+                <span>金额</span>
+                <span>期限</span>
+                <span>进度</span>
             </div>
-            <div class="loan">
-                <div class="title">最新投资</div>
-                <div class="list-head">
-                    <span>借款人</span>
-                    <span>借款标题</span>
-                    <span>年利率</span>
-                    <span>信用等级</span>
-                    <span>金额</span>
-                    <span>期限</span>
-                    <span>进度</span>
-                </div>
-                <ul>
-                <?php foreach ( $bids as $bid ):
-                foreach ( $bidProgressCssClassMap as $key => $bidProgressCssClass ){
-					if ( $bid['progress'] <= $key ){
-							$progressClass = $bidProgressCssClass;
-					}
-			}
-           ?>
-                    <li class="loan-list">
-                        <div class="loan-avatar">
-                            <img src="<?php echo $bid['userIcon']?>" />
-                            <span>信</span>
-                        </div>
-                        <div class="loan-title">
-                        	<a href="<?php echo $this->createUrl('tender/purchase/info',array('id'=>$bid['id']))?>" target="_blank">
-                        	<?php echo $bid['title']?>
-                        	</a>
-                        </div>
-                        <div class="loan-rate loan-num"><span class="val"><?php echo $bid['monthRate']?></span>%</div>
-                        <div class="loan-rank"><div><?php echo $bid['rank']?></div></div>
-                        <div class="loan-amount loan-num"><span class="val"><?php echo $bid['sum']?></span>元</div>
-                        <div class="loan-time loan-num"><span class="val"><?php echo $bid['deadline']?></span>个月</div>
-                        <div class="loan-progress">
-                            <div class="bar-out">
-                                <div class="bar-in">
-                                    <span class="bar-complete <?php echo $progressClass?>" style="width:<?php echo $bid['progress']?>%"></span>
-                                    <span class="bar-num"><?php echo $bid['progress']?>%</span>
-                                </div>
+            <ul>
+            <?php foreach ( $bids as $bid ):
+            foreach ( $bidProgressCssClassMap as $key => $bidProgressCssClass ){
+				if ( $bid['progress'] <= $key ){
+						$progressClass = $bidProgressCssClass;
+				}
+		}
+       ?>
+                <li class="loan-list">
+                    <div class="loan-avatar">
+                        <img src="<?php echo $bid['userIcon']?>" />
+                        <span>信</span>
+                    </div>
+                    <div class="loan-title">
+                    	<a href="<?php echo $this->createUrl('tender/purchase/info',array('id'=>$bid['id']))?>" target="_blank">
+                    	<?php echo $bid['title']?>
+                    	</a>
+                    </div>
+                    <div class="loan-rate loan-num"><span class="val"><?php echo $bid['monthRate']?></span>%</div>
+                    <div class="loan-rank"><div><?php echo $bid['rank']?></div></div>
+                    <div class="loan-amount loan-num"><span class="val"><?php echo $bid['sum']?></span>元</div>
+                    <div class="loan-time loan-num"><span class="val"><?php echo $bid['deadline']?></span>个月</div>
+                    <div class="loan-progress">
+                        <div class="bar-out">
+                            <div class="bar-in">
+                                <span class="bar-complete <?php echo $progressClass?>" style="width:<?php echo $bid['progress']?>%"></span>
+                                <span class="bar-num"><?php echo $bid['progress']?>%</span>
                             </div>
                         </div>
-                        <a href="<?php echo $this->createUrl('tender/purchase/info',array('id'=>$bid['id']))?>" target="_blank" class="invest">投标</a>
-                    </li>
-                <?php endforeach;?>
-                </ul>
-            </div>
-            <div class="platform">
-                <div class="title">传播平台</div>
-                <a href="#"><img src="<?php echo $this->imageUrl;?>android.png" id="android" /></a>
-                <a href="#"><img src="<?php echo $this->imageUrl;?>ios.png" id="ios"/></a>
-                <a href="#"><img src="<?php echo $this->imageUrl;?>wx.png" id="wx"/></a>
-            </div>
+                    </div>
+                    <a href="<?php echo $this->createUrl('tender/purchase/info',array('id'=>$bid['id']))?>" target="_blank" class="invest">投标</a>
+                </li>
+            <?php endforeach;?>
+            </ul>
+        </div>
+        <div class="platform">
+            <div class="title">传播平台</div>
+            <a href="#"><img src="<?php echo $this->imageUrl;?>android.png" id="android" /></a>
+            <a href="#"><img src="<?php echo $this->imageUrl;?>ios.png" id="ios"/></a>
+            <a href="#"><img src="<?php echo $this->imageUrl;?>wx.png" id="wx"/></a>
         </div>
     </div>
+    <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=574891711&site=qq&menu=yes" class="online_service" title="点击给我发送消息"></a>
