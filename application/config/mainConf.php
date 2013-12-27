8e81afbe575c30ce0d9a619e0e71da7e19c1e41d
root <root@backend.(none)>
2013-12-28 03:33:03 +0800

    x

--- application/config/mainConf.php
+++ application/config/mainConf.php
@@ -14,7 +14,7 @@ class mainConf extends ConfigBase{
 	
 	public function merge(){
 		return array(
-				'hostName' => 'http://localhost',
+				'hostName' => 'http://www.shanddai.com',
 				'preloadModels' => array(),
 				'modules' => array(
 						'pay',
