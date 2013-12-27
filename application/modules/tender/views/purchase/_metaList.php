e5a2c2d28ea21732b2ad11e9b54a6dd0ec010e64
toruneko <toruneko@outlook.com>
2013-12-27 22:42:52 +0800

    x

--- application/modules/tender/views/purchase/_metaList.php
+++ application/modules/tender/views/purchase/_metaList.php
@@ -11,6 +11,6 @@
 	<td><?php echo $data->getRelated('user')->getAttribute('nickname');?></td>
 	<td><?php echo number_format($data->getAttribute('sum') / 100,2);?></td>
 	<td><?php echo number_format($data->getAttribute('refund') / 100,2);?></td>
-	<td><?php echo $data->getAttribute('finish_time') ? date('Y-n-j H:i:s',$data->getAttribute('finish_time')) : '等待付款';?></td>
+	<td><?php echo $data->getAttribute('pay_time') ? date('Y-n-j H:i:s',$data->getAttribute('pay_time')) : '等待付款';?></td>
 	<!-- <td>什么意思？？</td>-->
 </tr>
