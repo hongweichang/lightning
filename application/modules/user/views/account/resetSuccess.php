26e542a60c1af0cbb570b18cc910bf04d5e2b7d9
Lancelot <lancelot1215@gmail.com>
2013-12-29 19:19:59 +0800

    x

---
+++ application/modules/user/views/account/resetSuccess.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+</head>
+<body>
+<script type="text/javascript">
+	alert("修改成功");
+	window.location.href="<?php echo $this->app->getBaseUrl();?>";
+</script>
+</body>
---
+++ application/views/layouts/blank.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+</head>
+<body>
+<?php
+/**
+ * @name blank.php
+ * @author lancelot <lancelot1215@gmail.com>
+ * Date 2013-12-29 
+ * Encoding UTF-8
+ */
+echo $content;
+?>
+</body>
