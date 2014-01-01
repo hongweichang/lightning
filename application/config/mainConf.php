20c4f3ac69d271b4f6d74a49e3ad4501a70be5f2
toruneko <toruneko@outlook.com>
2014-01-01 23:06:00 +0800

    x

--- application/config/mainConf.php
+++ application/config/mainConf.php
@@ -84,7 +84,7 @@ class mainConf extends ConfigBase{
 						'request' => array(
 								'class' => 'application.components.Request'
 						),
-						'cache' => array(
+						/*'cache' => array(
 								'class' => 'CMemCache',
 								'useMemcached' => true,
 								'keyPrefix' => 'lightning',
@@ -106,7 +106,7 @@ class mainConf extends ConfigBase{
 								//'cacheID' => 'cache',
 								'autoStart' => true,
 								'timeout' => 3600*24
-						),
+						),*/
 						'clientScript' => array(
 								'scriptMap' => array(
 										'jquery.js' => false,
@@ -134,7 +134,7 @@ class mainConf extends ConfigBase{
 										)
 								),
 						),
-						'zmqClient' => array(
+						/*'zmqClient' => array(
 								'class' => 'cms.components.asyncEvent.ZMQClient',
 								'zmqServer' => 'tcp://localhost:5556',
 								'sendTimeout' => 3000,
@@ -146,7 +146,7 @@ class mainConf extends ConfigBase{
 								'sendTimeout' => 3000,
 								'reciveTimeout' => 3000,
 						),
-						'asyncEventRunner' => require dirname(__FILE__).'/asyncEventRunner.config.php',
+						'asyncEventRunner' => require dirname(__FILE__).'/asyncEventRunner.config.php',*/
 						'image'=>array(
 								'class'=>'ext.image.CImageComponent',
 								'driver'=>'GD',
