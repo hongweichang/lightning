bacb3cff9c97e0072be7acbc5653e3ba225fc959
Lancelot <lancelot1215@gmail.com>
2014-01-17 14:35:06 +0800

    x

---
+++ application/controllers/ErrorController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @name ErrorController.php
+ * @author lancelot <lancelot1215@gmail.com>
+ * Date 2014-1-17 
+ * Encoding UTF-8
+ */
+class ErrorController extends Controller{
+	public function actionIndex(){
+		
+	}
+}
