32bfd2ce5391d94e26ea8882ea5fa46344b88e31
toruneko <toruneko@outlook.com>
2014-01-17 19:04:10 +0800

    x
    x

--- application/modules/pay/controllers/IpsController.php
+++ application/modules/pay/controllers/IpsController.php
@@ -24,7 +24,7 @@ class IpsController extends PayController{
 		$ips = $this->module->ips;
 		$ips['Billno'] = $order->getAttribute('id');
 		$ips['Amount'] = number_format($amount,2,".","");
-		//$ips['Attach'] = '闪电贷：'.$amount.'元（含手续费：'.($order->getAttribute('fee') / 100).'元）';
+		$ips['Attach'] = '闪电贷:'.$amount.'元(含手续费:'.($order->getAttribute('fee') / 100).'元)';
 		$ips['Date'] = date('Ymd');
 		unset($ips['Mer_key']);
 		
@@ -40,8 +40,8 @@ class IpsController extends PayController{
 		if($result){
 			if($this->getQuery('succ') == 'Y'){
 				$this->trade_no = $this->getQuery('ipsbillno');
-				//$this->subject = urldecode($this->getQuery('attach'));
-				$this->buyer = $this->getQuery('msg');
+				$this->subject = urldecode($this->getQuery('attach'));
+				$this->buyer = urldecode($this->getQuery('msg'));
 				$this->buyer_id = $this->getQuery('bankbillno');
 				//;
 				if($this->beginPay($this->getQuery('billno'))){
@@ -94,8 +94,8 @@ class IpsController extends PayController{
 				end if
 				**/
 				$this->trade_no = $this->getQuery('ipsbillno');
-				//$this->subject = urldecode($this->getQuery('attach'));
-				$this->buyer = $this->getQuery('msg');
+				$this->subject = urldecode($this->getQuery('attach'));
+				$this->buyer = urldecode($this->getQuery('msg'));
 				$this->buyer_id = $this->getQuery('bankbillno');
 				if($this->beginPay($this->getQuery('billno')) && $this->afterPay($this->getQuery('billno'))){
 					echo "ipscheckok";
