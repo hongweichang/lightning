f6cf47fb9157057387096a83a64f1dc4abf95173
Lancelot <lancelot1215@gmail.com>
2014-01-21 02:30:05 +0800

    x

--- application/modules/user/views/account/resetSuccess.php
+++ application/modules/user/views/account/resetSuccess.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 	<meta charset="UTF-8" />
+	<meta name="renderer" content="webkit"> 
+      <title>修改成功</title>
 	<script type="text/javascript" src="<?php echo $this->scriptUrl; ?>jquery-1.8.2.min.js"></script>
 	<script type="text/javascript">
 	$(document).ready(function() {
@@ -11,5 +13,5 @@
 	</script>
 </head>
 <body>
-
 </body>
+</html>
