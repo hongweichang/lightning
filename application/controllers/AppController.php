335e9ceb1f379caa127fd3682f646593ea17b165
Lancelot <lancelot1215@gmail.com>
2014-01-22 00:22:43 +0800

    x

--- application/controllers/AppController.php
+++ application/controllers/AppController.php
@@ -11,7 +11,7 @@ class AppController extends Controller{
 	}
 	
 	public function actionAndroid(){
-		$this->request->xSendFile('/app/shandiandai.apk',array(
+		$this->request->xSendFile('/appdownload/shandiandai.apk',array(
 			'xHeader' => 'X-Accel-Redirect'	
 		));
 	}
